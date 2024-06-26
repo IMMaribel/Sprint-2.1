3f96751edb21e9546d46ecbf68bafa0723678174
I Mマリベル <immaribel7@gmail.com>
2024-06-26 13:53:47 +0200

    Ej1 - conversión de funciones

---
+++ Ej1 - Arrow functions.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Regular Function
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// console.log(add(5,4));
+
+
+
+// Arrow function
+
+let add = (a,b) => a + b;
+
+console.log(add(6,13));
