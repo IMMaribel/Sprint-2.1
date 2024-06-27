316414af6acc0309b3ba90cc4d05cac67d41bd5c
I Mマリベル <immaribel7@gmail.com>
2024-06-27 13:47:44 +0200

    Ej2 - Operadores de comparación

--- Ej2 - Operador ternario.js
+++ Ej2 - Operador ternario.js
@@ -2,7 +2,7 @@
 
 // EJERCICIO 1.2: OPERADOR TERNARIO
 
-// *Ej1 - Operador ternario básico
+// *EJ1 - Operador ternario básico
 
 function potConduir(edad){
     if(edad >= 18){
@@ -15,3 +15,16 @@ function potConduir(edad){
 potConduir(4);
 potConduir(18);
 potConduir(23);
+
+// *EJ2 - Operadores de comparación
+
+function numeroMayor(num1, num2){
+    if(num1 > num2){
+        console.log(`${num1} es más grande que ${num2}.`);
+    }else{
+        console.log(`${num2} es más grande que ${num1}.`);
+    }
+}
+
+numeroMayor(12, 5);
+numeroMayor(10, 33);
