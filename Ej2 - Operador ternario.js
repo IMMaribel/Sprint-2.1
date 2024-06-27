d4aec079c20f06b014c405fd9829006e74fb2eee
I Mマリベル <immaribel7@gmail.com>
2024-06-27 20:07:55 +0200

    Ej3 - Uso enlazado de operadores y operador con funciones

--- Ej2 - Operador ternario.js
+++ Ej2 - Operador ternario.js
@@ -28,3 +28,27 @@ function numeroMayor(num1, num2){
 
 numeroMayor(12, 5);
 numeroMayor(10, 33);
+
+// *EJ3 - Uso enlazado de operadores ternarios
+
+let resultado = (numero) => console.log((numero > 0) ? 'Positivo' : (numero < 0) ? 'Negativo' : 'Cero');
+
+resultado(5);
+resultado(-3);
+resultado(0);
+
+// *EJ3 - Operador ternario con funciones
+
+function trobarMaxim(a,b,c){
+    if(a > b && a > c){
+        console.log(`${a} es el número mayor.`);
+    } else if(b > a && b > c){
+        console.log(`${b} es el número mayor.`); 
+    }else{
+    console.log(`${c} es el número mayor.`);
+    }
+}
+
+trobarMaxim(8,5,1);
+trobarMaxim(3,6,2);
+trobarMaxim(2,5,9);
