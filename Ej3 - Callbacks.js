880a8ff84f6d6ce68ea59064b5904922b9039c13
I Mマリベル <immaribel7@gmail.com>
2024-06-28 18:55:01 +0200

    Ej2 - Callbacks con operaciones matemáticas

--- Ej3 - Callbacks.js
+++ Ej3 - Callbacks.js
@@ -2,14 +2,23 @@
 
 // EJERCICIO 1.2: CALLBACKS
 
-//  *EJ1- Callback básico
+//  *EJ1 - Callback básico
 
 function procesar(number, Callback){
     Callback(number);
-
 }
 
 procesar(5, function(n){
     console.log(`El número es: ${n}.`);
 });
 
+// *EJ2 - Callbacks con operaciones matemáticas
+
+function calculadora(num1, num2, Callback){
+    Callback(num1, num2);
+}
+
+calculadora(2, 8, function(num1, num2){
+    let suma = num1 + num2;
+    console.log(`La suma de ${num1} y ${num2} es: ${suma}.`)
+});
