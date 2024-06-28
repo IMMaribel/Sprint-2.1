791386202f43488e0c911d986af19f7127825381
I Mマリベル <immaribel7@gmail.com>
2024-06-28 19:11:23 +0200

    Ej3 - Uso de callbacks en funciones asíncronas

--- Ej3 - Callbacks.js
+++ Ej3 - Callbacks.js
@@ -22,3 +22,15 @@ calculadora(2, 8, function(num1, num2){
     let suma = num1 + num2;
     console.log(`La suma de ${num1} y ${num2} es: ${suma}.`)
 });
+
+// *EJ3 - Uso de callbacks en funciones asíncronas
+
+function esperarISaludar(name,Callback){
+    setTimeout(() => {
+        Callback(name);
+    }, 2000);
+}
+
+esperarISaludar('Maribel', function(n){
+    console.log(`${n}.`);
+});
