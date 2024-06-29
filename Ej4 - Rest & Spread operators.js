de669642da056a1df8a8e375c29827cd253d3edd
I Mマリベル <immaribel7@gmail.com>
2024-06-29 12:00:57 +0200

    Ej2 - Operador Rest en Funciones

--- Ej4 - Rest & Spread operators.js
+++ Ej4 - Rest & Spread operators.js
@@ -7,3 +7,16 @@ let array2 = [3,2,1,0];
 let array3 = [...array1, ...array2];
 
 console.log(array3);
+
+// *EJ2 - Operador Rest en Funciones
+
+function suma(...num){
+    let total = 0;
+    for (const arg of num){
+        total += arg;
+    }
+    return total;
+}
+
+console.log(suma(5,2,8,0,3,4,12));
+console.log(suma(1,1,3));
