bef3ccb1159ebda52ce3ac668a83a8e6ff5dce2c
I Mマリベル <immaribel7@gmail.com>
2024-06-30 10:04:29 +0200

    Ej2 - Filter

--- Ej5 - Array transformations.js
+++ Ej5 - Array transformations.js
@@ -9,3 +9,13 @@ let alCuadrado = arrMap.map(function (x){
 
 console.log(arrMap);
 console.log(alCuadrado);
+
+// *EJ2 - Filter
+
+let arrFilter = [1, 2, 3, 4];
+let numPares = arrFilter.filter(function(y){
+    return y % 2 === 0;
+});
+
+console.log(arrFilter);
+console.log(numPares);
