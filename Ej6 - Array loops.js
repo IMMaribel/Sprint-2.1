7613be963f2ded62e401a6602bab66b89dabf8a8
I Mマリベル <immaribel7@gmail.com>
2024-07-01 11:41:31 +0200

    Ej3 - Filter

--- Ej6 - Array loops.js
+++ Ej6 - Array loops.js
@@ -9,3 +9,11 @@ nombres.forEach((element) => console.log(element));
 // *EJ2 - for-of
 
 for (let value of nombres) {console.log(value)};
+
+// *EJ3 - Filter
+
+const numeros = [1, 2, 3, 4, 5, 6];
+
+let pares = numeros.filter ((numeros) => numeros % 2 === 0);
+
+console.log(pares);
